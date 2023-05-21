91aece2d3156b95227f9d0ed7a3b1f06ded7e1df
Dyamez <dyamez@outlook.com>
2023-05-21 14:52:13 -0700

    052123-6

--- gameJs/index.js
+++ gameJs/index.js
@@ -6,3 +6,18 @@ let mensahe = ""
 let kathangTalata = document.getElementById("talata")
 let kalahatan = document.getElementById("sumaTotal")
 let mgaBaraha = document.getElementById("baraha") 
+
+function karambola() {
+    let randomNumber = Math.floor( Math.random() * 13) + 1
+    return randomNumber >  10 ? 10 : randomNumber === 1 ? 11 : randomNumber 
+}
+
+function bagongLaro() {
+    buhay = true;
+    let unangBaraha = karambola();
+    let pangalawangBaraha = karambola();
+    tumpok = [unangBaraha + pangalawangBaraha]
+    let kabuuan = unangBaraha + pangalawangBaraha
+    simulaLaro()
+}
+function simulaLaro()
