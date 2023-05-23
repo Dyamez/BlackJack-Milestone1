18b45428161995b37829cccc86d99218093a08b2
Dyamez <dyamez@outlook.com>
2023-05-23 12:34:16 -0700

    052323-2

--- gameJs/index.js
+++ gameJs/index.js
@@ -1,3 +1,5 @@
+
+
 let tumpok = []
 let kabuuan = 0
 let panalo = false
@@ -5,7 +7,11 @@ let buhay = false
 let mensahe = ""
 let kathangTalata = document.getElementById("talata")
 let kalahatan = document.getElementById("sumaTotal")
-let mgaBaraha = document.getElementById("baraha") 
+let mgaBaraha = document.getElementById("baraha")
+let mgaSugarol = document.getElementById("sugarol") 
+
+let manlalaro = { pangalan: "Edward", barya: 430 }
+mgaSugarol.textContent = manlalaro.pangalan = ": $" + manlalaro.barya 
 
 function karambola() {
     //return randomNumber >  10 ? 10 : randomNumber === 1 ? 11 : randomNumber 
@@ -20,23 +26,23 @@ function karambola() {
 }
 
 function bagongLaro() {
-    buhay = true;
+    buhay = true
     let unangBaraha = karambola();
     let pangalawangBaraha = karambola();
-    tumpok = [unangBaraha + pangalawangBaraha]
+    tumpok = [unangBaraha, pangalawangBaraha]
     kabuuan = unangBaraha + pangalawangBaraha
-    bagongLaro()
+    simula()
 }
-function bagongLaro() {
+function simula() {
     mgaBaraha.textContent = "Cards: "
     for (let i = 0; i < tumpok.length; i++) {
         mgaBaraha.textContent += tumpok[i] + " "
     }
-    kalahatan.textContent = "Sum: ", + kabuuan
+    kalahatan.textContent = "Sum: ", + kabuuan    //!
     if (kabuuan <= 20) {
         mensahe = "Pick a new card"
     } else if (kabuuan === 21) {
-        mensahe = " Hurray, You've got Blackjack!"
+        mensahe = " Hurray, its Blackjack!"
         panalo = true
     } else {
         mensahe = "Better luck next time!"
@@ -49,6 +55,6 @@ function bagongBalasa() {
         let tumpoks = karambola()
         kabuuan += tumpoks
         tumpok.push(tumpoks)
-        bagongLaro()
+        simula()
     }
 }
