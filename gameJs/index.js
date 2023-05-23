ac1b3be5e12b59376b0603ba060a5821b59660d5
Dyamez <dyamez@outlook.com>
2023-05-23 02:48:39 -0700

    052323-1

--- gameJs/index.js
+++ gameJs/index.js
@@ -8,8 +8,15 @@ let kalahatan = document.getElementById("sumaTotal")
 let mgaBaraha = document.getElementById("baraha") 
 
 function karambola() {
-    let randomNumber = Math.floor( Math.random() * 13) + 1
-    return randomNumber >  10 ? 10 : randomNumber === 1 ? 11 : randomNumber 
+    //return randomNumber >  10 ? 10 : randomNumber === 1 ? 11 : randomNumber 
+    let randomNumber = Math.floor( Math.random()*13 ) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
 }
 
 function bagongLaro() {
@@ -17,7 +24,31 @@ function bagongLaro() {
     let unangBaraha = karambola();
     let pangalawangBaraha = karambola();
     tumpok = [unangBaraha + pangalawangBaraha]
-    let kabuuan = unangBaraha + pangalawangBaraha
-    simulaLaro()
+    kabuuan = unangBaraha + pangalawangBaraha
+    bagongLaro()
 }
-function simulaLaro()
+function bagongLaro() {
+    mgaBaraha.textContent = "Cards: "
+    for (let i = 0; i < tumpok.length; i++) {
+        mgaBaraha.textContent += tumpok[i] + " "
+    }
+    kalahatan.textContent = "Sum: ", + kabuuan
+    if (kabuuan <= 20) {
+        mensahe = "Pick a new card"
+    } else if (kabuuan === 21) {
+        mensahe = " Hurray, You've got Blackjack!"
+        panalo = true
+    } else {
+        mensahe = "Better luck next time!"
+        buhay = false
+    }
+    kathangTalata.textContent = mensahe 
+} 
+function bagongBalasa() {
+    if (buhay === true && panalo === false) {
+        let tumpoks = karambola()
+        kabuuan += tumpoks
+        tumpok.push(tumpoks)
+        bagongLaro()
+    }
+}
