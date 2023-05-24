3f43c0a63ccddb7bb9ddb6f42a8eb6b86963b353
Dyamez <dyamez@outlook.com>
2023-05-24 00:34:01 -0700

    052423-1

--- gameJs/index.js
+++ gameJs/index.js
@@ -1,4 +1,3 @@
-
 let kathangTalata = document.getElementById("talata")
 let kalahatan = document.getElementById("sumaTotal")
 let mgaBaraha = document.getElementById("baraha")
@@ -15,14 +14,12 @@ let kalawakan = {
     mensahe : "",
 }
 
-function karambola() {
-    
+ let karambola = () => {
     let randomNumber = Math.floor( Math.random()*13 ) + 1
     return randomNumber >  10 ? 10 : randomNumber === 1 ? 11 : randomNumber 
-    
 }
 
-function bagongLaro() {
+ bagongLaro = ()=>{
     kalawakan.buhay = true
     let unangBaraha = karambola();
     let pangalawangBaraha = karambola();
@@ -30,7 +27,8 @@ function bagongLaro() {
     kalawakan.kabuuan = unangBaraha + pangalawangBaraha
     simula()
 }
-function simula() {
+
+ let simula = ()=>{
     mgaBaraha.textContent = "Your Hand: "
     for (let i = 0; i < kalawakan.tumpok.length; i++) {
         mgaBaraha.textContent += kalawakan.tumpok[i] + " "
@@ -47,7 +45,8 @@ function simula() {
     }
     kathangTalata.textContent = kalawakan.mensahe 
 } 
-function bagongBalasa() {
+
+ bagongBalasa = ()=>{
     if (kalawakan.buhay === true && kalawakan.panalo === false) {
         let tumpok = karambola()
         kalawakan.kabuuan += tumpok
@@ -55,61 +54,3 @@ function bagongBalasa() {
         simula()
     }
 }
-
-
-/*
-let kathangTalata = document.getElementById("talata")
-let kalahatan = document.getElementById("sumaTotal")
-let mgaBaraha = document.getElementById("baraha")
-let mgaSugarol = document.getElementById("sugarol") 
-
-let manlalaro = { pangalan: "🤑JavaNerd", barya: 223 }
-mgaSugarol.textContent = manlalaro.pangalan + ": 💰" + manlalaro.barya 
-
-let tumpok = []
-let kabuuan = 0
-let panalo = false
-let buhay = false
-let mensahe = ""
-
-function karambola() {
-    
-    let randomNumber = Math.floor( Math.random()*13 ) + 1
-    return randomNumber >  10 ? 10 : randomNumber === 1 ? 11 : randomNumber 
-    
-}
-
-function bagongLaro() {
-    buhay = true
-    let unangBaraha = karambola();
-    let pangalawangBaraha = karambola();
-    tumpok = [unangBaraha, pangalawangBaraha]
-    kabuuan = unangBaraha + pangalawangBaraha
-    simula()
-}
-function simula() {
-    mgaBaraha.textContent = "Your Hand: "
-    for (let i = 0; i < tumpok.length; i++) {
-        mgaBaraha.textContent += tumpok[i] + " "
-    }
-    kalahatan.textContent = "Result: " + kabuuan    
-    if (kabuuan <= 20) {
-        mensahe = "Pick a new card"
-    } else if (kabuuan === 21) {
-        mensahe = " Hurray, its Blackjack!"
-        panalo = true
-    } else {
-        mensahe = "Better luck next time!"
-        buhay = false
-    }
-    kathangTalata.textContent = mensahe 
-} 
-function bagongBalasa() {
-    if (buhay === true && panalo === false) {
-        let tumpoks = karambola()
-        kabuuan += tumpoks
-        tumpok.push(tumpoks)
-        simula()
-    }
-}
-*/
