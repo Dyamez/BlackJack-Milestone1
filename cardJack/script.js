931ec966c941a381fbe398adc5747e880deb3fed
Dyamez <dyamez@outlook.com>
2023-05-26 21:02:02 -0700

    052623-7

--- cardJack/script.js
+++ cardJack/script.js
@@ -1,12 +1,12 @@
-let unangBilang = 0;
-let akingBilang = 0;
-let sumaTangero = 0;
-let baraha = 0;
+let unangBilang = 0; //dealerAC
+let akingBilang = 0; //myS
+let sumaTangero = 0; //dealerS
+let baraha = 0; //myAC
 let patok = true;
 let tago;
 let tumpok;
 
-let umpisaLaro = ()=>{
+let latagan = ()=>{
     tumpok = []
     let kahulugan = ["C", "D", "H", "S"];
     let uri = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
@@ -26,7 +26,27 @@ let karambola = ()=>{
     console.log(tumpok);
 }
 
-()=>{
+let umpisaLaro = ()=>{
+    tago = tumpok.pop();
+    sumaTangero += kuhaSagot(tago);
+    unangBilang += kuhaTangero(tago);
+    while (sumaTangero < 17) {
+        let tumpokPik = document.createElement("img");
+        let papel  = tumpok.pop();  //card
+        tumpokPik.src = ".assets/" + papel + "png";
+        sumaTangero += kuhaSagot(papel);
+        unangBilang += kuhaTangero(papel);
+        document.getElementById("tangero").append(tumpokPik)
+    }
+
+
+
+
+
+
+
+
+
     document.getElementById("tago").src = ".assets/" + tago + ".png";
     patok = false;
     sumaTangero = 
@@ -37,7 +57,9 @@ document.getElementById("sumaTangero").innerText = sumaTangero;
 document.getElementById("baraha").innerText = baraha;
 document.getElementById("sumaTotal").innerText = mensahe;
 
-window.onload = ()=>{umpisaLaro(), karambola(), bagongLaro()}
+window.onload = ()=>{umpisaLaro(), karambola(), latagan()}
 
-
+let kuhaSagot = ()=>{
+    let laman = 
+}
 
