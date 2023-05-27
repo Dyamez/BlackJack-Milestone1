bab74e10bf16fe66604d9371994b6223a3b9b616
Dyamez <dyamez@outlook.com>
2023-05-26 20:18:24 -0700

    052623-5

--- cardJack/script.js
+++ cardJack/script.js
@@ -1,25 +1,9 @@
-#tangero img {
-    margin: 2px;
-    height: 170px;
-    width: 120px;
-}
-#baraha img {
-    margin: 2px;
-    height: 170px;
-    width: 120px;
-}
-#umpisa {
-    font-size: 25px;
-    height: 60px;
-    width: 110px;
-}
-@kuha {
-    font-size: 25px;
-    height: 60px;
-    width: 110px;
-}
-body {
-    font-family: "Arial"
-    text-align: clearInterval;
-    font-weight: bold
-}
+let sumaTangero = 0;
+let baraha = 0;
+
+
+
+
+document.getElementById("sumaTangero").innerText = dealerSum;
+document.getElementById("baraha").innerText = yourSum;
+document.getElementById("sumaTotal").innerText = mensahe;
---
+++ cardJack/styles.css
@@ -0,0 +1,25 @@
+#tangero img {
+    margin: 2px;
+    height: 170px;
+    width: 120px;
+}
+#baraha img {
+    margin: 2px;
+    height: 170px;
+    width: 120px;
+}
+#umpisa {
+    font-size: 25px;
+    height: 60px;
+    width: 110px;
+}
+@kuha {
+    font-size: 25px;
+    height: 60px;
+    width: 110px;
+}
+body {
+    font-family: "Arial"
+    text-align: clearInterval;
+    font-weight: bold
+}
